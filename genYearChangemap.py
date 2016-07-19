b79d0389fd29893e45aa424d04d7f60f06918bb5
HelenRouty <helen.youshan@gmail.com>
2016-07-19 17:25:41 -0500

    executeModel half done

--- genYearChangemap.py
+++ genYearChangemap.py
@@ -19,23 +19,12 @@ import sys, os
 from leamsite import LEAMsite
 from StringIO import StringIO
 import csv
+from Utils import createdirectorynotexist
+from subprocess import check_call
 
+scenatriotitle = 'test2_projection'
 resultsdir = 'http://portal.leam.illinois.edu/chicago/luc/scenarios/test4_scenario'
-
-def grassConfig(location='grass', mapset='model',
-    gisbase='/usr/local/grass-6.4.5svn', gisdbase='.'):
-    """ Set grass environment to run grass.script as grass
-    """
-    os.environ['GISBASE'] = gisbase
-    os.environ['GRASS_VERBOSE'] = '0'
-    os.environ['GRASS_OVERWRITE'] = '1'
-    sys.path.append(os.path.join(gisbase, 'etc', 'python'))
-
-    global grass
-    __import__('grass.script')
-    grass = sys.modules['grass.script']
-    from grass.script.setup import init
-    gisrc = init(gisbase, gisdbase, location, mapset)   
+DEMANDGRAPH ='gluc/Data/demand.graphs'
 
 def getDemandYearPop(demandstr, demandname):
     """@input: demandstr: StringIO gotten by getURL from leamsite.
@@ -62,41 +51,98 @@ def interpolate(x1, x2, y1, y2):
     results = []
     delta = float(y2-y1)/float(x2-x1)
     for i in xrange(0,x2-x1):
-        results.append((x1+i, int(round(y1+i*delta, 0))))
-    results.append((x2,y2))
+        results.append((x1+i, int(round(i*delta, 0))))
+    results.append((x2,y2-y1))
     return results
 
-def yearchange(yearpoplist):
-    grassConfig()
-    # how this have been done? where percell maps come from?
-    # --regional: subregional: processProjections...
-    # input = '%s,%s' % (time, year)? where it comes from?
-    grass.mapcalc('int1=int($mult*$percell)', percell='ppcell',
-                  mult=10000.0, quiet=True)
-    stats = grass.read_command('r.stats', flags='c', quiet=True,
-                               input='int1')
-    print stats
-    for l in stats.splitlines():
-        # not valid...
-        t,val,count = l.split()
-        print t, " ", val, " ", count
-        ts[int(t)]=ts.get(int(t),0.0)+int(val)*int(count)/mult
-        print t[int(t)]
+def writeDemand(yearincrpoplist, yearincremplist, 
+    title=scenatriotitle, filename=DEMANDGRAPH):
+    # concatenate the year, population to string to demand.graph file.
+    outstrlist = ['# %s\n\nPopulation\n'%title]
+    for yr, pop in yearincrpoplist:
+        outstrlist.append('%s, %s\n' % (str(yr), str(pop)))
+    outstrlist.append('\n\nEmployment\n')
+    for yr, emp in yearincremplist:
+        outstrlist.append('%s, %s\n' % (str(yr), str(emp)))
+
+    createdirectorynotexist(filename)
+    with open(filename, 'w') as f:
+        f.write(''.join(outstrlist))
+
+def genYearlyDemandGraph():
+    # fetch demand table from website
+    demandurl = "http://portal.leam.illinois.edu/chicago/luc/projections/test2_projection/getGraph"
+    demandstr = site.getURL(demandurl).getvalue()
+    
+    # parse the demand table to year and population and interpolate
+    yr1, yr2, pop1, pop2 = getDemandYearPop(demandstr, 'Population')
+    yearincrpoplist = interpolate(yr1, yr2, pop1, pop2)
+
+    yr1, yr2, emp1, emp2 = getDemandYearPop(demandstr, 'Employment')
+    yearincremplist = interpolate(yr1, yr2, emp1, emp2)
+
+    # output increment demand per year relative to the start year.
+    writeDemand(yearincrpoplist, yearincremplist)
+
+def genBilGLUCInputs():
+    grass.run_command('r.out.gdal', input='boundary', 
+        output='gluc/Data/boundary.bil', format='EHdr', type='Byte')
+    grass.run_command('r.out.gdal', input='landcoverBase',
+        output='gluc/Data/landcover.bil', format='EHdr', type='Byte')
+    grass.run_command('r.out.gdal', input='nogrowth', 
+        output='gluc/Data/nogrowth.bil', format='EHdr', type='Byte')
+    grass.run_command('r.out.gdal', input='pop_density', 
+        output='gluc/Data/pop_density.bil', format='EHdr',type='Float32')
+    grass.run_command('r.out.gdal', input='emp_density', 
+        output='gluc/Data/emp_density.bil', format='EHdr', type='Float32') 
+
+def executeGLUCModel(mode='growth', projid=scenatriotitle):
+    """***GLUC model requires inputs***
+       1. ${projid}.conf configuration file --- writeConf
+       2. demand.graph                      --- genYearlyDemandGraph
+       3. boundary, landcover, nogrowth, pop_density, emp_density
+          .bil && .hdr files                --- genBilGLUCInputs
+       4. empty change and summary .bil && .hdr files 
+                                            --- gluc.make start
+       ***GLUC model run***
+       gluc.make growth merge
+
+       ***GLUC model outputs***
+       change.bil and summary.bil
+    """
+    # ***GLUC model requires inputs*** #
+    # 1. writeConf --- TODO: later
+
+    # 2. generate demand.graph: yearly demand list
+    genYearlyDemandGraph()
+
+    # 3. generate bil files as gluc inputs
+    genBilGLUCInputs()
+
+    # 4. empty change and summary files
+    cmd = 'make -f ./bin/gluc.make start'
+    check_call(cmd.split())
+
+    # ***GLUC Model run*** #
+    try:
+        with open('./gluc/%s_gluclog.log' % scenatriotitle, 'w') as log:
+            cmd='make -f ./bin/gluc.make %s merge PROJID=%s'% (mode, projid)
+            check_call(cmd.split(), stdout=log, stderr=log)
+    except subprocess.CalledProcessError:
+        print 'GLUC Model Failure'
+        sys.exit(5)
 
 def main():
-    # global site
-    # site = LEAMsite(resultsdir, user=sys.argv[1], passwd=sys.argv[2])
-    # demandurl = "http://portal.leam.illinois.edu/chicago/luc/projections/test2_projection/getGraph"
-    # demandstr = site.getURL(demandurl).getvalue() # the demand is of type StringIO
+    global site
+    site = LEAMsite(resultsdir, user=sys.argv[1], passwd=sys.argv[2])
+
+    executeGLUCModel()
     
-    # yr1, yr2, pop1, pop2 = getDemandYearPop(demandstr, 'Population')
-    # yearpoplist = interpolate(yr1, yr2, pop1, pop2)
-    # print yearpoplist
-
-    yearchange([])
-
-
-
+    # MAPS = './gluc/DriverOutput/Maps/'
+    # grass.run_command('r.in.gdal', overwrite=True, input=MAPS+'change.bil' output=scenatriotitle+'_change'
+    # r.mapcalc "change=if(${PROJID}_change,${PROJID}_change,change)"
+    # r.in.gdal --overwrite input=${MAPS}/summary.bil output=${PROJID}_summary
+    
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
